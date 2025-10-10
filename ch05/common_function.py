68ccafbe084798870ad2c6aaba02d0995a624211
214 <214@gmail.com>
2025-10-10 11:23:53 +0900

    ch05 실습파일

--- ch05/common_function.py
+++ ch05/common_function.py
@@ -1,7 +1,8 @@
 import platform
+from matplotlib import rc
+from matplotlib import pyplot as plt
 
 #현재 파이썬이 실행되는 OS확인
-
 def is_windows_platform():
     return platform.system() == 'Windows'
 
@@ -18,5 +19,12 @@ def get_font_name():
         return 'linuxfont!!'
     else:
         return 'Malgun Gothic'
+
+#한글폰트 초기화
+def init_plt():
+    # 한글폰트설정
+    rc('font', family=get_font_name())
+    plt.rcParams['axes.unicode_minus'] = False
+
 
 
--- ch05/sample02.py
+++ ch05/sample02.py
@@ -12,14 +12,14 @@ plt.rcParams['axes.unicode_minus'] = False
 
 jumsu1 = [3.5, 4.0, 4.5, 3.8]
 jumsu2 = [3.0, 4.5, 4.0, 3.2]
-jumsu3 = [2.0, 3.0, 2.0, 4.0]
+#jumsu3 = [2.0, 3.0, 2.0, 4.0]
 year = [2024, 2025, 2026, 2027]
 
 jumsu_df = pd.DataFrame(
     {
         '홍길동': jumsu1,
         '이순신': jumsu2,
-        '이도': jumsu3,
+        #'이도': jumsu3,
     }, index = year)
 jumsu_df.plot.line()
 plt.show()
